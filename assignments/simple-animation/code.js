8d38f461bb4216cd3487edcd543b7711766c9b6d
Alexandermasters71 <111394826+Alexandermasters71@users.noreply.github.com>
2022-08-25 15:12:55 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -7,7 +7,7 @@
 const drawFrame = (time) => {
   clear();
   drawFilledCircle((time / 1) % width, height / 5, 45, 'blue');
-  drawFallingTriangle(width / 2, 3);
+  drawFallingTriangle(width / 2, 15);
 };
 
 // This is a function that we define to make it easier to draw a triangle. You
