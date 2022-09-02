18e4a430ae868a8f86b3c7e7e627fc3195eb15c4
Alexandermasters71 <111394826+Alexandermasters71@users.noreply.github.com>
2022-09-02 15:20:01 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -50,6 +50,14 @@ DrawFilledcircle((time / 120) % width, height/2,40, 'purple');
 
 drawballandline((time / 12) % width, (time/ 15) % height, 25, 'blue');
 
+const drawBall = (x1, y1, x2, y2, time, color) => {
+  const d = distance(x1, y1, x2, y2);
+  const p = fromStart(d, time, 2500);
+  const x = x1 + p / d * (x2 - x1);
+  const y = y1 + p / d * (y2 - y1);
+  drawFilledCircle(x, y, 10, color);
+};
+
 
 
 
