be8571c10e2962f159bbf465a7e6db885a9ce519
Alexandermasters71 <111394826+Alexandermasters71@users.noreply.github.com>
2022-09-07 15:28:10 -0700

    Updating assignments/boolean-birds/code.js

--- assignments/boolean-birds/code.js
+++ assignments/boolean-birds/code.js
@@ -38,5 +38,5 @@ const eatsWorms = (FlobbyBirds, BloggyBirds, FlibbleBirds, GlobbyBirds) => {
 }
 
 const eatsNuts = (FlobbyBirds, BloggyBirds, FlibbleBirds, GlobbyBirds) => {
-  return FlobbyBirds &&! FlibbleBirds || BloggyBirds || GlobbyBirds
+  return FlobbyBirds || BloggyBirds || GlobbyBirds &&! FlibbleBird
 }
