80bf2e67171b349f134c7981b282f67e94155e81
Alexandermasters71 <111394826+Alexandermasters71@users.noreply.github.com>
2022-09-12 15:05:07 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -1,39 +1,39 @@
-const add =(a,b) =>  {
-  return a+b;
+const add = (a, b) => {
+  return a + b;
 };
 
-const subtract =(a,b) =>  {
-  return a-b;
+const subtract = (a, b) => {
+  return a - b;
 };
 
-const multiply =(a,b) =>  {
-  return a*b;
-}; 
-
-const divide =(a,b) =>  {
-  return a/b;
+const multiply = (a, b) => {
+  return a * b;
 };
 
-const mod =(a,b) =>  {
-  return a%b;
+const divide = (a, b) => {
+  return a / b;
 };
 
-const averageOfThree =(a,b,c) =>  {
-  return (a+b+c)/3;
+const mod = (a, b) => {
+  return a % b;
 };
 
-const averageOfTwo =(a,b) =>  {
-  return (a+b)/2;
+const averageOfThree = (a, b, c) => {
+  return (a + b + c) / 3;
 };
 
-const distance =(x1,y1) =>  {
-  return distance.abs(x1-y1);
+const averageOfTwo = (a, b) => {
+  return (a + b) / 2;
 };
 
-const manhattanDistance =(x1,y1,x2,y2) =>  {
-  return Math.abs (y1-y2) + Math.abs(x1-x2);
+const distance = (x1, y1) => {
+  return Math.abs(x1 - y1);
 };
 
-const euclideanDistance =(x1,y1,x2,y2) =>  {
-  return Math.sqrt ((y2-y1)**2  + (x2-x1)**2) ; 
+const manhattanDistance = (x1, y1, x2, y2) => {
+  return distance(y1, y2) + distance(x1, x2);
+};
+
+const euclideanDistance = (x1, y1, x2, y2) => {
+  return Math.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2);
 };
