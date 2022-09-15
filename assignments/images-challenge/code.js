db83fe74364dadbdfd6cf9ce81f8595279dd2321
Alexandermasters71 <111394826+Alexandermasters71@users.noreply.github.com>
2022-09-15 14:59:48 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -28,6 +28,6 @@
 
  const fillwithcircles = (w) => {
 for (let x = 2; x < 100; x++) 
- return drawCircle(x*10, x*10, 8, 'blue', 1)
+ return drawCircle(x+10, x+10, 8, 'blue', 1)
 }
 fillwithcircles();
