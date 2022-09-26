39bf044636fd1d14908ad6c21922c873a718b58c
Alexandermasters71 <111394826+Alexandermasters71@users.noreply.github.com>
2022-09-26 15:14:25 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -28,9 +28,8 @@
 
 
 
-
+const numbCircles = Math.Floor(width / d);
 const fillwithcircles = (r) => {
-  const numbCircles = Math.Floor(width / d);
   const d = r * 2
   for (let x = 0; x < numbCircles; x++)
     if (x > length) {
