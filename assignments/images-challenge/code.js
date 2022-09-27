a33a933d32f7166cb411a40fcbf6fb28ed6552e5
Alexandermasters71 <111394826+Alexandermasters71@users.noreply.github.com>
2022-09-27 15:19:50 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -30,12 +30,12 @@
 let d = 1
 const numbCircles = Math.floor(width / d)
 const fillwithcircles = (r) => {
-  let y = 2
   let y = r * 2
-  for (let x = 0; x < numbCircles; x++)
+  for (let x = 0; x < numbCircles; x++) {
     if (x > length) {
-      drawFilledCircle(x* 30 , height / 2, 15, 'red')
+      drawFilledCircle(numbCircles-1 , height / 2, 15, 'red')
     }
 } 
+}
  fillwithcircles(12);
 
