3cf9a9df66ba5e8d73a7873b9eb3b5ca68b9a116
Alexandermasters71 <111394826+Alexandermasters71@users.noreply.github.com>
2022-10-26 14:39:02 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -12,11 +12,11 @@ registerOnclick((x, y) => {
 const drawBoard = () => {
   for (let i = 0; i < 2; i++) {
     const x = (i + 1) * width / 3
-    drawLine(x, 0, x, height, 'red', 5);
+    drawLine(x, 0, x, height, 'black', 5);
  }
   for ( let i = 0; i < 2; i++) {
     const x = (i + 1) * height / 3
-    drawLine(0, x, width, x, 'red', 5);
+    drawLine(0, x, width, x, 'black', 5);
   }
 }
 drawBoard()
