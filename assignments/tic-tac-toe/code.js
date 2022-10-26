8b8fbef89050b42e727edd9f4a6f15cf917f8289
Alexandermasters71 <111394826+Alexandermasters71@users.noreply.github.com>
2022-10-26 14:51:32 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -13,16 +13,16 @@ const drawBoard = () => {
   for (let i = 0; i < 2; i++) {
     const x = (i + 1) * width / 3
     drawLine(x, 0, x, height, 'black', 5);
- }
-  for ( let i = 0; i < 2; i++) {
+  }
+  for (let i = 0; i < 2; i++) {
     const x = (i + 1) * height / 3
     drawLine(0, x, width, x, 'black', 5);
   }
 }
 drawBoard()
 
-const hitBox = [
-  [ '', '', '' ],
-  [ '', '', '' ],
-  [ '', '', '' ],
-];
+const hitBox = () => {
+  if (y < 3 && x < 3){
+    
+  }
+}
