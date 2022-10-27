5bbbe743bb32c771d92e289b7e9de24f198fa9c5
Alexandermasters71 <111394826+Alexandermasters71@users.noreply.github.com>
2022-10-27 15:19:21 -0700

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -2,10 +2,10 @@ const findFnord = (s) => {
   return s.indexOf('fnord')
 }
 
-const stringContains = (s1,s2) => {
-  if (s1.search(s2)){
-    return true
-  } else {
+const stringContains = (s1, s2) => {
+  if (s1.search(s2)) {
     return false
+  } else {
+    return true
   }
 }
