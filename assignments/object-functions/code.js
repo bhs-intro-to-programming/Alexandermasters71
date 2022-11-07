1407fc11d8a336eb46fe2ceac7fb9f1e733c751b
Alexandermasters71 <111394826+Alexandermasters71@users.noreply.github.com>
2022-11-07 15:41:15 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -18,6 +18,10 @@ const midpoint = (point1, point2) => {
   return { "x" :(point1.x + point2.x) / 2 , "y": (point1.y + point2.y) / 2 }
 }
 
-const sumSalaries = () => {
-  
+const sumSalaries = (array) => {
+  let add = 0
+  for (const element of array) {
+    add += element;
+  }
+  return add
 }
