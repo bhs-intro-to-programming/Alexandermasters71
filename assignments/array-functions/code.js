bd2b84fe2a54c41b4e45f4ba9d50282a0fe40925
Alexandermasters71 <111394826+Alexandermasters71@users.noreply.github.com>
2022-11-09 15:14:35 -0800

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,47 +1,6 @@
 const countTens = (array) => {
-  let num = 0
-  for (const element of array) {
-    if (element === 10) {
-      num++;
-    }
-  }
-  return num
-}
-
-
-const sum = (array) => {
-  let add = 0
-  for (const element of array) {
-    add += element;
-  }
-  return add
-}
-
-const evens = (array) => {
-  const evenArray = []
-  for (const element of array) {
-    if (element % 2 == 0) {
-      evenArray.push(element)
-    }
-  }
-  return evenArray
-}
-
-const anyOverOneHundred = (array) => {
-  for (const element of array) {
-    if (element > 100) 
-      return true
-  }
-   return false
-}
-
-
-const pyramid = (number) => {
-  const array = [] 
-  for (let x = 1; x <= number; x++){
-    for (let i = 1; i <= x; i++) {
-      array.push(x)
-    }
-  }
-  return array
+  let ten = 0
+  for(const element of array){
+    element === 10
+    return ten
 }
