970371ce64b50a8d66086567979126ca876764fe
Alexandermasters71 <111394826+Alexandermasters71@users.noreply.github.com>
2022-11-10 15:23:43 -0800

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -27,17 +27,11 @@ const evens = (array) => {
 }
 
 const anyOverOneHundred = (array) => {
-  let num = 0
   for (const element of array) {
     if (element > 100) {
-      num++;
-    }
-  }
-  if (num > 0) {
-    return true
-  } else {
+      return true
+      else 
     return false
-  }
-}
-
+    }
+    }
 
