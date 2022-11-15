8fb76bd2796560c4747a1bcf13461e437e897f8d
Alexandermasters71 <111394826+Alexandermasters71@users.noreply.github.com>
2022-11-15 15:08:28 -0800

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -17,6 +17,10 @@ drawText(WHITE_KING, width / 2, height / 2, 'black', 64);
 registerOnclick((x, y) => {
   let turn = "x"
 
+const d = r * 2
+  const numbCircles = Math.floor(width / d)
+  const offset = width - (d * numbCircles); 
+  
   const spacingH = height / 7;
   const box_y = Math.floor(y / spacingH)
 
@@ -39,3 +43,13 @@ const drawBoard = () => {
   }
 }
 drawBoard()
+
+
+const fillwithcircles = (r) => {
+  const d = r * 2
+  const numbCircles = Math.floor(width / d)
+  const offset = width - (d * numbCircles); 
+  for (let x = 0; x < numbCircles; x++) {
+    drawFilledCircle(offset + d * x, height / 2, r, 'red')
+  }
+}
