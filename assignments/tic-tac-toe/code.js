3abbce5a8fa5495be2c1d0fe0a17dfaeb829dcf3
Alexandermasters71 <111394826+Alexandermasters71@users.noreply.github.com>
2022-11-17 15:03:06 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -6,7 +6,7 @@
 // weeks but for now you can just adapt this code.
 
 const cordArray = [
-  ['', '', ''], 
+  ['', '', ''],
   ['', '', ''],
   ['', '', ''],
 ];
@@ -19,9 +19,11 @@ registerOnclick((x, y) => {
   const spacingW = width / 3;
   const box_x = Math.floor(x / spacingW)
 
-  drawText(turn, (box_x * (width / 3) + width / 6) - 50, (box_y * (height / 3) + height / 6) + 50, 'black', Math.min(width, height) * 0.3);
-  cordArray[box_y][box_x] = turn
-  turn = turn === "x" ? turn = "O" : turn = "x"
+  if (cordArray[box_y][box_x] = '') {
+    drawText(turn, (box_x * (width / 3) + width / 6) - 50, (box_y * (height / 3) + height / 6) + 50, 'black', Math.min(width, height) * 0.3);
+    cordArray[box_y][box_x] = turn
+    turn = turn === "x" ? turn = "O" : turn = "x"
+  }
 });
 
 const drawBoard = () => {
