c4d466ac19f83b824adb5145a55e8da59c10cb57
Alexandermasters71 <111394826+Alexandermasters71@users.noreply.github.com>
2022-11-17 15:03:31 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -19,7 +19,7 @@ registerOnclick((x, y) => {
   const spacingW = width / 3;
   const box_x = Math.floor(x / spacingW)
 
-  if (cordArray[box_y][box_x] = '') {
+  if (cordArray[box_y][box_x] === '') {
     drawText(turn, (box_x * (width / 3) + width / 6) - 50, (box_y * (height / 3) + height / 6) + 50, 'black', Math.min(width, height) * 0.3);
     cordArray[box_y][box_x] = turn
     turn = turn === "x" ? turn = "O" : turn = "x"
