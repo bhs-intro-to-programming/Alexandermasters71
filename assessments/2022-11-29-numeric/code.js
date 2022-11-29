187d26df54ea427be98a1503d4415e753b84fe5e
Alexandermasters71 <111394826+Alexandermasters71@users.noreply.github.com>
2022-11-29 14:40:25 -0800

    Updating assessments/2022-11-29-numeric/code.js

--- assessments/2022-11-29-numeric/code.js
+++ assessments/2022-11-29-numeric/code.js
@@ -2,3 +2,6 @@ const totalEggs = (hardBoiled,softBoiled) => {
   return hardBoiled + softBoiled
 }
 
+const extraChocolates = (numberOfPeople,numberOfChoclates) => {
+return numberOfPeople%numberOfChoclates
+}
