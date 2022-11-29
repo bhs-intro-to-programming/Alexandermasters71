d22697f92e1a805b6bdbf3e5cb22d17a2ae7ba53
Alexandermasters71 <111394826+Alexandermasters71@users.noreply.github.com>
2022-11-29 15:13:31 -0800

    Updating assessments/2022-11-29-numeric/code.js

--- assessments/2022-11-29-numeric/code.js
+++ assessments/2022-11-29-numeric/code.js
@@ -1,34 +1,38 @@
-const totalEggs = (hardBoiled,softBoiled) => {
+const totalEggs = (hardBoiled, softBoiled) => {
   return hardBoiled + softBoiled
 }
-const chocolatesPerPerson = (numberOfPeople,numberOfChoclates) => {
-  return Math.abs(numberOfPeople-numberOfChoclates)
+const chocolatesPerPerson = (numberOfPeople, numberOfChoclates) => {
+  return Math.abs(numberOfPeople - numberOfChoclates)
 }
 
-const extraChocolates = (numberOfPeople,numberOfChoclates) => {
-return numberOfChoclates%numberOfPeople
+const extraChocolates = (numberOfPeople, numberOfChoclates) => {
+  return numberOfChoclates % numberOfPeople
 }
 
-const leftOut = (numberOfPeople,numberOfChoclates) => {
-return numberOfChoclates/numberOfPeople
+const leftOut = (numberOfPeople, numberOfChoclates) => {
+  return numberOfChoclates / numberOfPeople
 }
 
 const probabilityAllHeads = (numberOfFlips) => {
-return 1/2 /(numberOfFlips)
+  return 1 / 2 / (numberOfFlips)
 }
 
-const futureHour= (currentHour,nextHours) => {
-return currentHour+(nextHours%24)
+const futureHour = (currentHour, nextHours) => {
+  return currentHour + (nextHours % 24)
 }
 
-const presentsBudget = (numberOfPresents,averagePrice) => {
-return numberOfPresents * averagePrice
+const presentsBudget = (numberOfPresents, averagePrice) => {
+  return numberOfPresents * averagePrice
 }
 
-const perPresent = (totalAmount,numberOfPresents) => {
-  return Math.floor(totalAmount/numberOfPresents)
+const perPresent = (totalAmount, numberOfPresents) => {
+  return Math.floor(totalAmount / numberOfPresents)
 }
 
-const wrappingCombos = (kindsOfPaper,kindsOfRibbons,kindsOfDecorBows) => {
-return kindsOfPaper*kindsOfRibbons*kindsOfDecorBows
+const wrappingCombos = (kindsOfPaper, kindsOfRibbons, kindsOfDecorBows) => {
+  return kindsOfPaper * kindsOfRibbons * kindsOfDecorBows
+}
+
+const biggestNumber = (numberOfDigits) => {
+  return (10**numberOfDigits)
 }
