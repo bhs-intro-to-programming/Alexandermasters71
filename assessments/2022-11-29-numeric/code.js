2e342e7e0577dea7580c9906576193e5e7f1168e
Alexandermasters71 <111394826+Alexandermasters71@users.noreply.github.com>
2022-11-29 15:16:23 -0800

    Updating assessments/2022-11-29-numeric/code.js

--- assessments/2022-11-29-numeric/code.js
+++ assessments/2022-11-29-numeric/code.js
@@ -34,5 +34,5 @@ const wrappingCombos = (kindsOfPaper, kindsOfRibbons, kindsOfDecorBows) => {
 }
 
 const biggestNumber = (numberOfDigits) => {
-  return (9**numberOfDigits) 
+  return (10**numberOfDigits)-1 
 }
