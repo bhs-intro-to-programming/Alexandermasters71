0c5b0067f54e319c494da706771b0e98fdb19957
Alexandermasters71 <111394826+Alexandermasters71@users.noreply.github.com>
2022-12-02 15:04:18 -0800

    Updating assessments/2022-12-02-booleans/code.js

--- assessments/2022-12-02-booleans/code.js
+++ assessments/2022-12-02-booleans/code.js
@@ -20,28 +20,35 @@ const needMittens = (goingOutside, inWarmEnviroment) => {
 }
 
 const isVenomous = (striped, blueHead) => {
-  if (striped ||  ! blueHead) {
+  if (striped || !blueHead) {
     return true
   }
   else return false
 }
 
-const okaySpeed = (speedLimit,carsSpeed) => {
-if (Math.abs(carsSpeed-speedLimit===10)){
-  return true
-}
-else return false
-}
-
-const twiceAsExpensive = (price,price1) => {
-if(price1*2===price){
-  return true
-}
-else return false
+const okaySpeed = (speedLimit, carsSpeed) => {
+  if (Math.abs(carsSpeed - speedLimit === 10)) {
+    return true
+  }
+  else return false
 }
 
-const winningRecord = (wins,losses,ties) => {
-if(wins * 1/2 >= (losses+ties)){
+const twiceAsExpensive = (price, price1) => {
+  if (price1 * 2 === price) {
+    return true
+  }
+  else return false
+}
+
+const winningRecord = (wins, losses, ties) => {
+  if (wins * 1 / 2 >= (losses + ties)) {
+    return true
+  }
+  else return false
+}
+
+const isMagicNumber = (n1) => {
+if(42||17){
   return true
 }
 else return false
