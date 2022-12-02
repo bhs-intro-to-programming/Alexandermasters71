a3d4efeeae9390c7e86b4cedc45516a50a7a7c6a
Alexandermasters71 <111394826+Alexandermasters71@users.noreply.github.com>
2022-12-02 14:43:57 -0800

    Updating assessments/2022-12-02-booleans/code.js

--- assessments/2022-12-02-booleans/code.js
+++ assessments/2022-12-02-booleans/code.js
@@ -1,20 +1,27 @@
-const needHeavyCoat = (goingOutside,isCold) => {
-  if(goingOutside && isCold){
+const needHeavyCoat = (goingOutside, isCold) => {
+  if (goingOutside && isCold) {
     return true
   }
   else return false
 }
 
-const needSunscreen = (goingToTheBeach,goingSkiing) => {
-if(goingToTheBeach || goingSkiing){
-return true
-}
-else return false
+const needSunscreen = (goingToTheBeach, goingSkiing) => {
+  if (goingToTheBeach || goingSkiing) {
+    return true
+  }
+  else return false
 }
 
-const needMittens = (goingOutside,inWarmEnviroment) => {
-if(goingOutside && ! inWarmEnviroment){
-return true
+const needMittens = (goingOutside, inWarmEnviroment) => {
+  if (goingOutside && !inWarmEnviroment) {
+    return true
+  }
+  else return false
 }
-else return false
+
+const isVenomous = (striped, blueHead) => {
+  if (striped && blueHead) {
+    return true
+  }
+  else return false
 }
