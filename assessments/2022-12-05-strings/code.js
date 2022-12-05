9ed7250f0b6f34a1287cc1a8ca7539fb9406bc34
Alexandermasters71 <111394826+Alexandermasters71@users.noreply.github.com>
2022-12-05 14:59:44 -0800

    Updating assessments/2022-12-05-strings/code.js

--- assessments/2022-12-05-strings/code.js
+++ assessments/2022-12-05-strings/code.js
@@ -5,3 +5,7 @@ return s.substring(0,s.indexOf('x'))
 const charactersAround = (s,n) => {
 return s.substring(n-1,n)+s.substring(n+1,s.length-n)
 }
+
+const middle = (s) => {
+return s.substring(2,s.length-2)
+}
