375b6a882e20a4669cb2c02c6a732a907bb9f0df
Alexandermasters71 <111394826+Alexandermasters71@users.noreply.github.com>
2022-12-08 14:42:05 -0800

    Updating assessments/2022-12-08-control-constructs/code.js

--- assessments/2022-12-08-control-constructs/code.js
+++ assessments/2022-12-08-control-constructs/code.js
@@ -1,6 +1,15 @@
 const yesIfEven = (n) => {
-if (n%2===0){
-  return "yes"
+  if (n % 2 === 0) {
+    return "yes"
+  }
+  else return "no"
 }
-else return "no"
+
+const countXs = (s) => {
+  for (let i = 0; i < s.length; i++) {
+    if (s.search('x')) {
+      i++
+    }
+  }
+  return i
 }
