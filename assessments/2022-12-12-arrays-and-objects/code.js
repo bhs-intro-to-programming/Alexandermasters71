42bc6bec53c19866a723da88ca5ab50cc62c8616
Alexandermasters71 <111394826+Alexandermasters71@users.noreply.github.com>
2022-12-12 15:08:54 -0800

    Updating assessments/2022-12-12-arrays-and-objects/code.js

--- assessments/2022-12-12-arrays-and-objects/code.js
+++ assessments/2022-12-12-arrays-and-objects/code.js
@@ -9,3 +9,16 @@ const makeBoard = () => {
 const makeMove = (markXOrO,rowNumber,columNumber) => {
   return  {'markXOrO' :xOrO,'rowNumber':rowSpecifier,'columNumber' :columSpecifier}
 }
+
+//const placeMark = () =>{
+  //makeMove() makeBoard()
+//}
+
+const allTheSame = (array) =>{
+  if(array[0]=== array[1]){
+    if(array[1]===array[2]){
+      return true
+    }
+    else return false
+  }
+}
