07c671e9260529a2c9470d135397883715a8047b
Alexandermasters71 <111394826+Alexandermasters71@users.noreply.github.com>
2022-12-12 15:18:40 -0800

    Updating assessments/2022-12-12-arrays-and-objects/code.js

--- assessments/2022-12-12-arrays-and-objects/code.js
+++ assessments/2022-12-12-arrays-and-objects/code.js
@@ -1,25 +1,33 @@
 const makeRow = () => {
-  return ['','','']
+  return ['', '', '']
 }
 
 const makeBoard = () => {
-  return ['','',''],['','',''],['','','']
+  return ['', '', ''], ['', '', ''], ['', '', '']
 }
 
-const makeMove = (markXOrO,rowNumber,columNumber) => {
-  return  {'markXOrO' :xOrO,'rowNumber':rowSpecifier,'columNumber' :columSpecifier}
+const makeMove = (markXOrO, rowNumber, columNumber) => {
+  return { 'markXOrO': xOrO, 'rowNumber': rowSpecifier, 'columNumber': columSpecifier }
 }
 
 //const placeMark = () =>{
-  //makeMove() makeBoard()
+//makeMove() makeBoard()
 //}
 
-const allTheSame = (array) =>{
-  if(array[0]=== array[1]){
-    if(array[1]===array[2]){
+const allTheSame = (array) => {
+  if (array[0] === array[1]) {
+    if (array[1] === array[2]) {
       return true
     }
-   
   }
   else return false
+}
+
+//const extractColumn = () => {
+
+//}
+
+const recordMove = (array,move) => {
+let array = [0,0,0,move]
+return array
 }
