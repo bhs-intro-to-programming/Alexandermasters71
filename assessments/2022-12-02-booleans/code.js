a0e9d00092a6b3390710b4ae912607c4ba0d1c74
Alexandermasters71 <111394826+Alexandermasters71@users.noreply.github.com>
2022-12-16 10:51:31 -0800

    Updating assessments/2022-12-02-booleans/code.js

--- assessments/2022-12-02-booleans/code.js
+++ assessments/2022-12-02-booleans/code.js
@@ -1,3 +1,7 @@
 const needHeavyCoat = (isgoingout,somewherecold) =>{
 return isgoingout && somewherecold
 }
+
+const needSunscreen = (goingtobeach,goingskiing) =>{
+return goingtobeach || goingskiing
+}
