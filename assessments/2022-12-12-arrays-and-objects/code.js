5e52177f8073c820be280be2b3f764764603849e
Alexandermasters71 <111394826+Alexandermasters71@users.noreply.github.com>
2022-12-16 11:03:07 -0800

    Updating assessments/2022-12-12-arrays-and-objects/code.js

--- assessments/2022-12-12-arrays-and-objects/code.js
+++ assessments/2022-12-12-arrays-and-objects/code.js
@@ -1,33 +1,3 @@
-const makeRow = () => {
-  return ['', '', '']
-}
-
-const makeBoard = () => {
-  return ['', '', ''], ['', '', ''], ['', '', '']
-}
-
-const makeMove = (markXOrO, rowNumber, columNumber) => {
-  return { 'markXOrO': xOrO, 'rowNumber': rowSpecifier, 'columNumber': columSpecifier }
-}
-
-//const placeMark = () =>{
-//makeMove() makeBoard()
-//}
-
-const allTheSame = (array) => {
-  if (array[0] === array[1]) {
-    if (array[1] === array[2]) {
-      return true
-    }
-  }
-  else return false
-}
-
-const extractColumn = (array,index0To2) => {
-return array[index0To2,0] +array[index0To2,1]+array[index0To2,2]
-}
-
-const recordMove = (array,move) => {
- array = [0,0,0,move.x+move.y]
-return array
+const makeRow = () =>{
+  return [ '','','']
 }
